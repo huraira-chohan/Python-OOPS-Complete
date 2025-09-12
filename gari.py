59ae6405b2750a4a9138b7ffa491ee35fd71734e
Huraira <abuhurairachohan2006@gmail.com>
2025-09-12 21:46:20 +0500

    Basic OOPs Done

---
+++ gari.py
@@ -0,0 +1,21 @@
+# gari ki class
+class gari:
+
+    def __init__(self):
+        # gari ky attributes
+        self.color = 'red'
+        self.milage = 500
+        self.engine = 'V12'
+
+        # gari ka method
+
+    def milage_eng(self):
+        print(self.milage,self.engine)
+
+
+# gari ka object
+bugatti = gari()
+print(bugatti.engine)
+    
+bugatti.milage_eng()
+
---
+++ oops1.py
@@ -0,0 +1,28 @@
+# initialize a class :
+
+class employee :
+    # initialize special magic/dunder method - constructor
+    # attributes are called by themselves
+    def __init__(self):
+        print("attributes being initiated")
+        self.id = 123
+        self.salary = 5000
+        self.dep = 'SDE'
+        print('attributes initiated')
+    # func made inside a class is called a method
+    # we call method manually
+    def travel(self,destination):
+         print("Manually Initialized")
+         print(f"Employee is going to {destination}")
+
+# Make an obj of the class :
+sam = employee()
+
+# Calling Attributes :
+print(sam.salary)
+# sam.id
+# sam.dep
+
+# Calling a method :
+print(sam.travel('isl'))
+
