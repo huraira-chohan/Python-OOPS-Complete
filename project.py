91e33216d5e318fd11682e13b1f07f1ff7f7f7eb
Huraira <abuhurairachohan2006@gmail.com>
2025-09-13 14:30:48 +0500

    Project basics done

---
+++ project.py
@@ -0,0 +1,31 @@
+class chohanbook:
+    def __init__(self):
+        # attributes
+        self.username = ""
+        self.password = ""
+        self.messege = ""
+        self.post = ""
+        self.menu()
+
+        # method
+    def menu(self):
+        user_input = (input("""
+            Press 1 for signup.
+            Press 2 for signin.
+            Press 3 to post.
+            Press 4 to message a friend.
+            Press any other key to exit."""))
+        
+        if user_input == '1':
+             pass
+        elif user_input == '2':
+             pass
+        elif user_input == '3':
+             pass
+        elif user_input == '4':
+             pass
+        else:
+             pass
+
+x = chohanbook()
+    
